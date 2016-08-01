eadeed138a54a7c0f60bd2b695bd581157c8e0a3
monpeco <monpeco@gmail.com>
2016-08-01 22:16:01 +0000

    Update on timeDate.cpp, to check day, month and year

--- timeDate.cpp
+++ timeDate.cpp
@@ -4,15 +4,20 @@
 using namespace std;
 
 int main(){
-	int date, time, calendarYear, calendarDateAndTime, calendaDate, calendarTime;
+	int date, time, calendarYear, calendarDayAndMonth, calendarMonth, calendarDay;
 	int hour, minute, hash;
 
 	cout << "Give date (DDMMYYYY) and time (HHMM): ";
 	cin >> date >> time;
 	calendarYear = date % 10000;
+	calendarDayAndMonth = date / 10000;
+	calendarMonth = calendarDayAndMonth % 100;
+	calendarDay = calendarDayAndMonth / 100;
 	cout <<  "calendarYear: " << calendarYear << endl;
+	cout <<  "calendarDayAndMonth: " << calendarDayAndMonth << endl;
+	cout <<  "calendarMonth: " << calendarMonth << endl;
+	cout <<  "calendarDay: " << calendarDay << endl;
 
-//	calendarYear = date % 10000;
 
 
 

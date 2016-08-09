890b70d0f8a0a37088f64a0a6bcac36c6dc24a92
monpeco <monpeco@gmail.com>
2016-08-09 22:51:39 +0000

    Update namingShel.cpp, now validates the ordenNumer and argument

--- namingShell.cpp
+++ namingShell.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
 #include <string.h> //strcpy
+#include <algorithm>
 using namespace std;
 
 
   
 int main (int argc, char* argv[]) {
-   char linea[1000] = "";
-   char nombre[100] = "";
-   int i = 0;
-   cout << "argv[1]: " << argv[1] << endl;
+    char linea[1000] = "";
+    char nombre[100] = "";
+    string orden = argv[2];
+    string argumento = argv[3];
    
+   int i = 0;
    while(argv[1][i] != '.'){
         nombre[i] =  argv[1][i];
         i++;
    }
-   cout << "nombre: " << nombre << endl;
    
-   
-cout << "ORIGEN_PATH=`pwd`\n\
-EJECUTABLE=\"" << argv[1] << "\"\n\
+    size_t fOrden = orden.find("-");
+    cout << " fOrden: " << fOrden << endl ;
+    if (fOrden<8) orden.replace(fOrden, string("-").length(), "neg");
+
+    size_t fArgumento = argumento.find(" 12");    
+    if (fArgumento<100) argumento.replace(fArgumento, string(" 12").length(), " 05");
+
+cout << "\nORIGEN_PATH=`pwd`\n\
+EJECUTABLE=\"" << nombre << "\"\n\
 LOG_PATH=\"$ORIGEN_PATH\"\n\
 ARCHIVO_LOG=\"$LOG_PATH/$EJECUTABLE.log\"\n\
 echo \"===========================================================\" > $ARCHIVO_LOG\n\
 echo \"INICIO Proceso $EJECUTABLE.exe :\"`date \"+%d-%m-%Y at %H:%M\":%S` >> $ARCHIVO_LOG\n\
 echo \">>>> Ejecutando Proceso \" $EJECUTABLE\".exe\" >> $ARCHIVO_LOG\n\
-/home/batch/exp/expesval/obj/$EJECUTABLE.exe " << argv[2] << " >> $ARCHIVO_LOG\n\
+/home/batch/exp/expesval/obj/$EJECUTABLE.exe " << argumento << " >> $ARCHIVO_LOG\n\
 echo \"FIN Proceso $EJECUTABLE.exe :\"`date \"+%d-%m-%Y at %H:%M\":%S` >> $ARCHIVO_LOG\n\
 echo \"===========================================================\" >> $ARCHIVO_LOG\n\
 echo \"\" >> $ARCHIVO_LOG\n\n" << endl ;
+
+cout << " vi proceso" << orden << "_" << nombre << ".sh\n" << endl ;
    return 0;
 }

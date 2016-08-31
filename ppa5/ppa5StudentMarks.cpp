8d7df85542777a7a8fdcb5d8ec044b99c852259e
monpeco <monpeco@gmail.com>
2016-08-31 12:57:57 +0000

    Add comparator func, but nor working properly

--- ppa5/ppa5StudentMarks.cpp
+++ ppa5/ppa5StudentMarks.cpp
@@ -9,6 +9,29 @@ int index2            : Denotes the the row number(index) of the student.  */
 int comparator(float marksarray[][2],int index1, int index2) {
     //Write your code below this line to calculate RMS, compare them, and return either -1, 0, or 1  
 
+    float square_midsem_marks1 = pow(marksarray[index1][0],2);
+    float square_endsem_marks1 = pow(marksarray[index1][1],2);
+    float rms1 = sqrt(square_midsem_marks1 + square_endsem_marks1);
+    cout << "RMS of index1:" << rms1 ;
+
+    float square_midsem_marks2 = pow(marksarray[index2][0],2);
+    float square_endsem_marks2 = pow(marksarray[index2][1],2);
+    float rms2 = sqrt(square_midsem_marks2 + square_endsem_marks2);
+    cout << "\tRMS of index2:" << rms2 << endl;
+
+    if (rms1 > rms2){
+        return -1;
+    } else if (rms1 < rms2){
+        return 1;
+    } else {
+        if (marksarray[index1][1] > marksarray[index2][1]){
+            return -1;
+        } else if (marksarray[index1][1] < marksarray[index2][1]){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 
 }
 
@@ -40,25 +63,18 @@ int merge(float sortedarray[][2], float mergedarray[][2], int break_point, int n
 }
 
 int main(){
-    float marksarray[][2]={ {90, 30},
-{60, 20},
-{30, 40},
-{60, 20},
-{10, 90},
-{0, 100},
-{60, 60},
-{50, 10} };
+    float marksarray[][2]={ {90, 30}, {60, 20}, {30, 40}, {60, 20}, {10, 90}, {0, 100}, {60, 60}, {50, 10} };
     int index1=0, index2=0, numElements=8;
-    
-    for(int i=0;i<numElements;i++){
-        for(int j=0;j<2;j++){
+    int result = 0;
+    for(int i=0;i<numElements;i++,cout << endl)
+        for(int j=0;j<2;j++)
             cout << "\tmarksarray[" << i << "][" << j << "]:" << marksarray[i][j] ;
+            
+            
+    index1 = 0; index2 = 1;
+    result = comparator(marksarray, index1, index2);
+    cout << "result: " << result << endl ;
 
-        }        
-        cout << endl;
-    }
-    
-    
     cout << "End" << endl ;
     return 0;
 }

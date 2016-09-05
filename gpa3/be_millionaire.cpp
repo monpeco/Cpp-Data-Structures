d77431e7edd2ff31014cbcb287795e146a39ec1a
monpeco <monpeco@gmail.com>
2016-09-05 13:58:11 +0000

    Move be_millonaiere.cpp to folder

---
+++ gpa3/be_millionaire.cpp
@@ -0,0 +1,37 @@
+
+
+/* Question        : Compare and compare the ecodes of 'index1' and 'index2' and return -1, 0, or 1
+int audience [][2] : Contains the base and exponent.
+int index1         : Denotes the row numberin the array 'audience'.
+int index2         : Denotes the row numberin the array 'audience' */
+int comparator(int audience[][2],int index1, int index2) {
+    //Write your solution code below this line
+
+
+}
+
+
+/* Question        : Sort the array as described in the question (previous tab) on edX
+int audience [][2] : Contains the base and exponent.
+int N              : Denotes Number of rows in the array 'audience'.
+int &i_index       : Denotes the row numberin the array 'audience'.
+int &j_index       : Denotes the row numberin the array 'audience'. */
+void sorting(int audience[][2],int N, int &i_index, int &j_index){
+    //Write your solution code below this line
+    
+    
+}
+
+
+/* Question          : Merge the array as described in the question (previous tab) on edX
+int audience[][2]    : The array, now, Partially sorted using the function 'sorting' (called from the main)
+int mergedarray[][2] : To contain the final values after merging. This is passed as 'mergedmarks' from the main
+int N                : Denotes the number of rows in both the arrays given above. 
+int i_index          : Row number of the array 'audience' returned by the function 'sorting'
+int j_index          : Row number of the array 'audience' returned by the function 'sorting' */
+void merge(int audience[][2], int mergedarray[][2],int N, int i_index , int j_index) {
+
+    //Write your solution code below this line
+
+    
+}

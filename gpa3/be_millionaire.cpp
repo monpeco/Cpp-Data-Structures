bacbc4892c4fdad0c16417e525cb2379fd190fbd
monpeco <monpeco@gmail.com>
2016-09-13 20:19:13 +0000

    Update on be_millionaire, but not working properly

--- gpa3/be_millionaire.cpp
+++ gpa3/be_millionaire.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int ecode(int _nBase, int _nExponent){
+float ecode(int _nBase, int _nExponent){
     
     int nEcode = 1;
-    //cout << "------->_nBase: " << _nBase << endl;
-    //cout << "------->_nExponent: " << _nExponent << endl;
+    //cout << "------->_nBase: \t" << _nBase << endl;
+    //cout << "------->_nExponent: \t" << _nExponent << endl;
     
     for(int index=0; index < _nExponent; index++){
-        nEcode *= _nBase;
-        nEcode %= 100;
-        //cout << "------->nEcode: " << nEcode << endl;
+        nEcode = nEcode * _nBase;
+        nEcode = nEcode % 100;
     }
-    //cout << "========>nEcode: " << nEcode << endl;
 
-    return (nEcode % 100);
+    //cout << "========>nEcode: \t" << nEcode << endl;
+    //cout << "=================================\t" << endl;
+
+    return nEcode;
 }
 
 /* Question        : Compare and compare the ecodes of 'index1' and 'index2' and return -1, 0, or 1
@@ -44,28 +45,42 @@ int &j_index       : Denotes the row numberin the array 'audience'. */
 void sorting(int audience[][2],int N, int &i_index, int &j_index){
     //Write your solution code below this line
     
+    //cout << "------------------------------------------------ 0" << endl;
+    
     //Find the i_index value
     for(int index=0; index < N-1; index++){
         if(comparator(audience, index, index+1) == -1){
-            //cout << "into: " << index << endl;
             i_index = index;
+            //cout << "i_index: " << index << endl;
             break;
         }
     }
     
-    //cout << "------------------------------------------------ " << endl;
+    //cout << "------------------------------------------------ 1" << endl;
     
     //Find the j_index value
     for(int index=N-2; index > 0; index--){
         if(comparator(audience, index, index+1) == -1){
-            //j_index = index+1;
-            j_index = index;
-            //cout << "------------index: " << index << ", index+1: " << index+1 << endl;
+            j_index = index+1;
+            //j_index = index;
+            //cout << "j_index: " << j_index << endl;
             break;
         }
     }
     
+    //cout << "------------------------------------------------ 2" << endl;
     
+    //Sort the array between i_index and j_index
+    for(int index=i_index+1; index < j_index-1; index++){
+        if(comparator(audience, index, index+1) == -1){
+            int temp0 = audience[index][0];
+            int temp1 = audience[index][1];
+            audience[index][0] = audience[index+1][0];
+            audience[index][1] = audience[index+1][1]; 
+            audience[index+1][0] = temp0;
+            audience[index+1][1] = temp1;
+        }
+    }
     
 }
 
@@ -81,14 +96,15 @@ void merge(int audience[][2], int mergedarray[][2],int N, int i_index , int j_in
     //Write your solution code below this line
 
 int indexA = 0, indexB = j_index, indexM = 0;
-    cout << "------: " << endl;
+    //cout << "------: " << endl;
 
 for(indexA=0, indexB=j_index; ((indexA < i_index+1) || (indexB < N));){
-	    cout << "indexA: " << indexA;
+	    /*cout << "indexA: " << indexA;
 	    cout << "\tindexB: " << indexB;
 	    cout << "\tindexM: " << indexM;
 	    cout << "\ti_index: " << i_index;
-	    cout << "\ti_index+1: " << i_index+1 << endl;
+	    cout << "\ti_index+1: " << i_index+1 << endl;*/
+	    if (indexB > N) break;
 	    //break;
 	if((indexA < i_index+1) && (indexB < N)){
 		if(comparator(audience, indexA, indexB) == 1){
@@ -121,9 +137,9 @@ for(indexA=0, indexB=j_index; ((indexA < i_index+1) || (indexB < N));){
 int main (int argc, char* argv[]) {
     
     //int audience[][2] = {{6,13}, {5,3}, {43,2}, {3,8}, {7,23}, {5,3} , {9,92} , {5,3} , {11,26} , {4,23} };
-    int audience[][2] = { {9,92}, {100,80}, {4,62}, {1,44}, {96,41}, {6,92}, {85,73}, {67,28}, {12,34}, {2,4}, {27,41}, {22,2}, {83,10}, {27,54}, {48,9}, {94,32} };
-    int mergedarray[][2] = {{0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0} , {0,0} , {0,0} , {0,0} , {0,0} };
-    int index1 = 8, index2 = 1, comparator_result = -9, N = 16;
+    int audience[][2] = {{93,25}, {7,26}, {46,54}, {63,46}, {22,72}, {64,41}, {88,50}, {100,53}, {64,97}, {13,6}, {16,81}, {99,4}, {7,95}, {15,4}};
+    int mergedarray[][2] = {{0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0} , {0,0} , {0,0} , {0,0} , {0,0}, {0,0} , {0,0} , {0,0} , {0,0} , {0,0} };
+    int index1 = 8, index2 = 1, comparator_result = -9, N = 10;
     int i_index = 0, j_index = N;
     
     cout << "------------------------------------------------ " << endl;
@@ -140,15 +156,15 @@ int main (int argc, char* argv[]) {
         cout << " audience[" << index << "]:\t" << audience[index][0] << "\t," << audience[index][1] << endl ;
     }
     
-    cout << "------------------------------------------------ " << endl;
-    cout << "i_index: " << i_index << endl;
-    cout << "j_index: " << j_index << endl;
+    // cout << "------------------------------------------------ " << endl;
+    // cout << "i_index: " << i_index << endl;
+    // cout << "j_index: " << j_index << endl;
     
-    merge(audience, mergedarray, N, i_index , j_index);
-    cout << "------------------------------------------------ " << endl;
-    for (int index=0; index<N; index++){
-        cout << " mergedarray[" << index << "]: " << mergedarray[index][0] << "\t," << mergedarray[index][1] << endl ;
-    }
+    // merge(audience, mergedarray, N, i_index , j_index);
+    // cout << "------------------------------------------------ " << endl;
+    // for (int index=0; index<N; index++){
+    //     cout << " mergedarray[" << index << "]: " << mergedarray[index][0] << "\t," << mergedarray[index][1] << endl ;
+    // }
 
     cout << "End" << endl;
     return 0;

f06422775d5d0d498d65735a611931e38b3695f4
monpeco <monpeco@gmail.com>
2016-09-14 22:00:28 +0000

    Update on ffacsal_0026_s

--- ffacsal_0026_s.c
+++ ffacsal_0026_s.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int     cantFetch;
+int cantFetch, lFecIni, lFecFin, iCont;
+;
+long    LeeClientes;
 
 struct SALSSB0001
 {
@@ -25,13 +27,47 @@ struct SALSSB0001
 };
 struct SALSSB0001 *strSel01;
 
+FetchCursorPrincipal()
+{
+    LeeClientes= 0;
+    strSel01[0].cod_empresa = 1;
+    strSel01[0].nro_suministro = 234;
+    
+    strSel01[1].cod_empresa = 1;
+    strSel01[1].nro_suministro = 678;
+    
+    strSel01[2].cod_empresa = 1;
+    strSel01[2].nro_suministro = 999;
+    
+    if(LeeClientes>0) return(1);
+    return(0);
+}
+
 main(argc,argv)
 int argc;
 char **argv;
 {
     cantFetch=100;
     strSel01=(struct SALSSB0001*)malloc(sizeof(struct SALSSB0001) * cantFetch);
-    
-    
+    lFecIni = 1;
+    lFecFin = 5;
+    while (lFecIni<=lFecFin)
+    {
+        while(FetchCursorPrincipal())
+        {
+            for(iCont=0;iCont<LeeClientes;iCont++)
+            {
+                
+            }
+        }
+        lFecIni++;
+    }
+        
+    printf("cod_empresa:\t[%d]\n", strSel01[0].cod_empresa);
+    printf("nro_suministro: [%ld]\n", strSel01[0].nro_suministro);
+    printf("cod_empresa:\t[%d]\n", strSel01[1].cod_empresa);
+    printf("nro_suministro: [%ld]\n", strSel01[1].nro_suministro);
+    printf("cod_empresa:\t[%d]\n", strSel01[2].cod_empresa);
+    printf("nro_suministro: [%ld]\n", strSel01[2].nro_suministro);    
     printf("OK\n");
 }

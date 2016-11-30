f38bd7da3885c4e173a295e03377f6ed8a8413b7
monpeco <monpeco@gmail.com>
2016-11-30 12:49:57 +0000

    Add car object (prototype and implementation)

---
+++ object_oriented/car.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "car.h"
+using namespace std;
+
+Car::Car(){
+    cout << "Car constructor";
+}
+
+Car::~Car(){
+    cout << "Car constructor";
+}
+
+int main()
+{
+    return 0;
+}

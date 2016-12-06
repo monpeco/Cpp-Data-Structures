5a4b172621f3194df4be4c80ac2fcc75204ab74f
monpeco <monpeco@gmail.com>
2016-12-06 21:28:01 +0000

    Restore regular compiler

--- c_mix_cpp/compiler.sh
+++ c_mix_cpp/compiler.sh
@@ -4,8 +4,7 @@ c_file=$1
 cpp_file=$2
 echo "Compile start"
 
-#gcc -c -o $c_file.o $c_file.c
-gcc -Wall -ggdb -c -o $c_file.o $c_file.c
+gcc -c -o $c_file.o $c_file.c
 g++ -c -o $cpp_file.o $cpp_file.cpp   
 g++ -o program $c_file.o $cpp_file.o  
 

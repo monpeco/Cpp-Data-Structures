ba73c7cbd5688494f4474af86396008416a5af4f
monpeco <monpeco@gmail.com>
2016-12-16 12:36:33 +0000

    Use of friendship with a entire class

--- object_oriented/friend_function.cpp
+++ object_oriented/friend_function.cpp
@@ -6,15 +6,26 @@ class Animal{
         double weight;
         double height;
     public:
-        Animal(double w, double h);
+        Animal(double w, double h){
+            weight = w;
+            height = h;
+        };
         friend void printWeight(Animal animal);
+        friend class AnimalPrinter;
 
 };
 
-Animal::Animal(double w, double h){
-    weight = w;
-    height = h;
-}
+
+class AnimalPrinter
+{
+public:
+    void print(const Animal& animal)
+    {
+        // Because of the `friend class AnimalPrinter;" declaration, we are
+        // allowed to access private members here.
+        std::cout << "Inside AnimalPrinter.print(): " << animal.weight << ", " << animal.height << std::endl;
+    }
+};
 
 void printWeight(Animal animal)
 {
@@ -25,5 +36,6 @@ void printWeight(Animal animal)
 int main() {
     Animal animal(10,20);
     printWeight(animal);
-
+    AnimalPrinter animalPrinter;
+    animalPrinter.print(animal);
 }

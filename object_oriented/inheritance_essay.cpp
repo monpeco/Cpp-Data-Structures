d5f2c8a6944c1d69622e7a40f6bd58bccba40afb
monpeco <monpeco@gmail.com>
2016-12-16 15:44:38 +0000

    Teacher inherit from Person (public inheritance)

--- object_oriented/inheritance_essay.cpp
+++ object_oriented/inheritance_essay.cpp
@@ -12,8 +12,14 @@ struct Person
 };
 
 //Make Teacher inherit publicly (default) from Person
-struct Teacher : Person
+struct Teacher : public Person // equivalent struct Teacher : Person
 {
+    void foo()
+    {
+        id = 2;         //well formed, id is public in Teacher
+        heigth = 170;   //well formed, heigth is protected in Teacher
+        //weigth = 80;    bad formed, because weight is private in Teacher
+    }    
 };
 
 
@@ -21,4 +27,10 @@ int main(){
     Person mike;
     mike.id = 51;
     cout << "Person.id: " << mike.id << endl;
+    
+    Teacher john;
+    john.foo();
+    cout << "Teacher.id: " << john.id << endl;
+    //cout << "Teacher.heigth: " << john.heigth;  bad formed, because weight is protected and it cant be accessed from outside of the class Teacher
+
 }

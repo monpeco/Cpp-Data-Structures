dd0d4fde59ff885a69098e87ad30c950edd44787
monpeco <monpeco@gmail.com>
2016-12-17 22:07:21 +0000

    Update on circular buffer example to see index/position

--- misc/circular_buffer.cpp
+++ misc/circular_buffer.cpp
@@ -15,7 +15,12 @@ int main()
 {
     for(int i = 0; i<11;i++){
         cout << "i%4=[" << i%4 << "]" << endl;
-
+        sprintf(lecturas[i%4].C016_lectura_2, "posicion numero %d, indice %d", i%4, i);
     }
+    
+    for(int i = 0; i<4;i++){
+        cout << "[" << lecturas[i%4].C016_lectura_2 << "]" << endl;
+    }    
+    
     return 0;
 }

9f5894b10ccbda25abfae5c65094bd3abad77d65
monpeco <monpeco@gmail.com>
2016-12-19 21:57:41 +0000

    Update virtual inheritance to instaciate D object

--- object_oriented/virtual_inheritance.cpp
+++ object_oriented/virtual_inheritance.cpp
@@ -11,22 +11,30 @@ struct A
     void foo() { cout << "foo: [" << member << "]\n"; }
 };
 
-struct B: virtual A
+struct B: virtual public A
 {
     B(): A(5){}
 };
 
-struct C: virtual A
+struct C: virtual public A
 {
     C(): A(88) {}
 };
 
-
+struct D : public B, public C
+{
+    D(): A(200) {}
+    void bar()
+    {
+        foo(); //Without virtual inheritance this would be an Error, which foo? B::foo() or C::foo()? - Ambiguous
+    }
+};
 
 int main()
 {
     B first;    //constructor: [5]
     C second;   //constructor: [88]
-
+    D third;    //constructor: [200]
+    
     return 0;
 }

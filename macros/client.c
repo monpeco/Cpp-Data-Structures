c538f534b1ad6e60696b5af2bead055a9c3170d0
monpeco <monpeco@gmail.com>
2017-01-03 21:53:12 +0000

    Add MY_MULT macro (recieve 2 parameters)

--- macros/client.c
+++ macros/client.c
@@ -14,10 +14,13 @@ int main ()
     value = CURRENT;
     printf ("(with CURRENT) -> value: %d\n", value);
 
-
+    value = MY_MULT (5, 10);
+    printf ("(with MY_MULT) -> value: %d\n", value);
+    
     /* Output:
     (with MY_NULL) -> num: (nil)
     (with CURRENT) -> value: 4
+    (with MY_MULT) -> value: 50
     */
     return 0;
 }
--- macros/symbols.h
+++ macros/symbols.h
@@ -2,3 +2,7 @@
 #define MY_NULL (void *)0
 #endif // #ifndef MY_NULL
 
+#ifndef MY_MULT
+#define MY_MULT(x, y) (x) * (y)
+#endif // #ifndef MY_MULT
+
